d020f0ed42fa8db680f005360449aee49562fc5c
DESKTOP-6FVFHUO\기훈 <tjsrlgns97@gmail.com>
2025-12-23 01:33:30 +0900

    feat - 기훈 -Myinfo page 기본구조 및 레이아웃

--- app/myinfo/page.tsx
+++ app/myinfo/page.tsx
@@ -1,5 +1,29 @@
-const MyInfo = () => {
-  return <div>MyInfo page</div>;
-};
+// app/myinfo/page.tsx (또는 dashboard/page.tsx)
+"use client";
 
-export default MyInfo;
+import { useState } from "react";
+import Sidebar from "@/feature/MyInfo/components/Sidebar";
+import MyInfoView from "@/feature/MyInfo/MyInfoView";
+import ReservationView from "@/feature/MyInfo/ReservationView";
+import MyExperinenceView from "@/feature/MyInfo/MyExperinenceView";
+import ReservaionStatusView from "@/feature/MyInfo/ReservaionStatusView";
+import type { SidebarMenu } from "@/types/SidebarTypes";
+
+export default function MyInfoPage() {
+  const [activeMenu, setActiveMenu] = useState<SidebarMenu>("MY_INFO");
+
+  return (
+    <div className="flex min-h-screen">
+      <div className="flex ml-30 xl:ml-117.5 pt-10">
+        <Sidebar active={activeMenu} onChange={setActiveMenu} />
+
+        <main className="flex-1 p-6">
+          {activeMenu === "MY_INFO" && <MyInfoView />}
+          {activeMenu === "RESERVATIONS" && <ReservationView />}
+          {activeMenu === "MY_EXPERIENCE" && <MyExperinenceView />}
+          {activeMenu === "RESERVATION_STATUS" && <ReservaionStatusView />}
+        </main>
+      </div>
+    </div>
+  );
+}
---
+++ feature/MyInfo/components/Sidebar.tsx
@@ -0,0 +1,62 @@
+"use client";
+
+import { SIDEBAR_MENUS } from "@/feature/MyInfo/Utils/SidebarMenuConfig";
+import { SidebarProps } from "@/types/SidebarTypes";
+import ImageEditButton from "./ImageEditButton";
+import BasicImage from "@/public/Image/cloude.svg"
+import Image from "next/image";
+
+export default function Sidebar({ active, onChange, user }: SidebarProps) {
+  return (
+    <aside className="w-64 max-h-112.5 border border-gray-50 rounded-xl px-6 py-3.5 shadow">
+      <div className="flex items-center my-6 justify-center">
+        <div className="relative w-30 h-30">
+          
+          <div className="w-full h-full rounded-full bg-primary-100 overflow-hidden border border-gray-100">
+            {user?.profileImageUrl ? (
+              <Image
+                src={user.profileImageUrl}
+                alt={`${user?.nickname ?? "user"} profile`}
+                className="w-full h-full object-cover"
+              />
+            ) : (
+              <div className="w-full h-full flex items-center justify-center text-gray-400 text-sm">
+                <BasicImage/>
+              </div>
+            )}
+          </div>
+
+          <ImageEditButton />
+        </div>
+      </div>
+
+      <nav className="flex flex-col gap-3.5">
+        {SIDEBAR_MENUS.map(({ key, label, icon: Icon }) => {
+          const isActive = active === key;
+          return (
+            <button
+              key={key}
+              onClick={() => onChange(key)}
+              className={`flex items-center gap-2 px-3 py-2 rounded-md transition cursor-pointer
+                ${isActive ? "bg-blue-100" : "hover:bg-gray-100"}
+              `}
+            >
+              <Icon
+                className={`w-6 h-6 ${
+                  isActive ? "text-primary-500" : "text-gray-400"
+                }`}
+              />
+              <span
+                className={`text-sm font-medium ${
+                  isActive ? "text-gray-950" : "text-gray-600"
+                }`}
+              >
+                {label}
+              </span>
+            </button>
+          );
+        })}
+      </nav>
+    </aside>
+  );
+}
