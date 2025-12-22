53b4ee55492f0f919075535afb7a92c7167ddba0
Choi hee ruk <chlgmlfkr593@naver.com>
2025-12-23 03:26:33 +0900

    ✨ Feat: 전역 스타일을 포함한 기본 RootLayout 추가

---
+++ app/layout.tsx
@@ -0,0 +1,13 @@
+import "../style/global.css";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="ko">
+      <body>{children}</body>
+    </html>
+  );
+}
