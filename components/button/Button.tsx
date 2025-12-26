2c402a301dd5889c26dc7996a4540ac6d94127ad
DESKTOP-6FVFHUO\기훈 <tjsrlgns97@gmail.com>
2025-12-26 21:00:38 +0900

    버튼 컴포넌트 fowardref 제거 variant 추가

--- components/button/Button.tsx
+++ components/button/Button.tsx
@@ -1,45 +1,39 @@
-import { forwardRef } from "react";
+"use client";
+
+import { ReactNode } from "react";
 import ButtonContext from "./Button.context";
 import { ButtonVariants } from "./ButtonVariants";
 import { cn } from "@/lib/utils/twmerge";
 
-export interface ButtonProps
-  extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+interface ButtonProps {
   variant?: "primary" | "secondary" | "text" | "ghost";
   size?: "lg" | "md" | "sm";
-  full?: boolean;
+  disabled?: boolean;
+  children: ReactNode;
+  className?: string;
 }
 
-const ButtonBase = forwardRef<HTMLButtonElement, ButtonProps>(
-  (
-    {
-      className,
-      variant = "primary",
-      size = "md",
-      full = false,
-      disabled,
-      children,
-      ...props
-    },
-    ref,
-  ) => {
-    return (
-      <ButtonContext.Provider value={{ variant, size, disabled }}>
-        <button
-          ref={ref}
-          disabled={disabled}
-          className={cn(
-            ButtonVariants({ variant, size, full }),
-            className,
-          )}
-          {...props}
-        >
-          {children}
-        </button>
-      </ButtonContext.Provider>
-    );
-  },
-);
-
-ButtonBase.displayName = "Button";
-export default ButtonBase;
+export default function Button({
+  variant = "primary",
+  size = "md",
+  disabled = false,
+  children,
+  className,
+  ...props
+}: ButtonProps) {
+  return (
+    <ButtonContext.Provider value={{ variant, size, disabled }}>
+      <button
+        type="button"
+        disabled={disabled}
+        className={cn(
+          ButtonVariants({ variant, size }),
+          className
+        )}
+        {...props}
+      >
+        {children}
+      </button>
+    </ButtonContext.Provider>
+  );
+}
