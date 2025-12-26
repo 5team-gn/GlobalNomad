ccab9fab62820651505ba9e3cbed537eb005dd50
DESKTOP-6FVFHUO\기훈 <tjsrlgns97@gmail.com>
2025-12-26 22:26:39 +0900

    Button 구조 재구성

--- components/button/Index.ts
+++
@@ -1,8 +0,0 @@
-import ButtonRoot from "./Button";
-import ButtonIcon from "./Button.Icon";
-import ButtonLabel from "./Button.Label";
-
-export const Button = Object.assign(ButtonRoot, {
-  Icon: ButtonIcon,
-  Label: ButtonLabel,
-});
