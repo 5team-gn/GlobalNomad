1f99ee4f8c7e603381f61c6be6965089d8caf012
DESKTOP-6FVFHUO\기훈 <tjsrlgns97@gmail.com>
2025-12-26 22:29:01 +0900

    이미지 허용을 위한 config.ts 수정

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  images: {
+    domains: ["images.unsplash.com"],
+  },
+
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
