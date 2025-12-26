61ca3876c1dc70a6690242fcf2976307e56c3d7b
DESKTOP-6FVFHUO\기훈 <tjsrlgns97@gmail.com>
2025-12-26 21:04:34 +0900

    내정보페이지 layout변경

--- app/myinfo/page.tsx
+++ app/myinfo/page.tsx
@@ -12,16 +12,20 @@ export default function MyInfoPage() {
   const [activeMenu, setActiveMenu] = useState<SidebarMenu>("MY_INFO");
 
   return (
-    <div className="flex min-h-screen">
-      <div className="flex ml-30 xl:ml-117.5 pt-10">
-        <Sidebar active={activeMenu} onChange={setActiveMenu} />
+    <div className="min-h-screen ">
+      {/* 중앙 컨테이너 */}
+      <div className="mx-auto max-w-245 px-6 pt-10">
+        <div className="flex gap-12.5">
+          <Sidebar active={activeMenu} onChange={setActiveMenu} />
 
-        <main className="flex-1">
-          {activeMenu === "MY_INFO" && <MyInfoView />}
-          {activeMenu === "RESERVATIONS" && <ReservationView />}
-          {activeMenu === "MY_EXPERIENCE" && <MyExperinenceView />}
-          {activeMenu === "RESERVATION_STATUS" && <ReservaionStatusView />}
-        </main>
+          {/* 콘텐츠 영역 */}
+          <main className="flex-1 min-w-0">
+            {activeMenu === "MY_INFO" && <MyInfoView />}
+            {activeMenu === "RESERVATIONS" && <ReservationView />}
+            {activeMenu === "MY_EXPERIENCE" && <MyExperinenceView />}
+            {activeMenu === "RESERVATION_STATUS" && <ReservaionStatusView />}
+          </main>
+        </div>
       </div>
     </div>
   );
