0cc5861c49ea1f14b87e2586fa34c4d8238b2454
DESKTOP-6FVFHUO\기훈 <tjsrlgns97@gmail.com>
2025-12-26 22:06:21 +0900

    myinfo 페이지 쿼리스트링 및 상태적용

--- app/myinfo/page.tsx
+++ app/myinfo/page.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import { useState } from "react";
+import { useSearchParams, useRouter } from "next/navigation";
+
 import Sidebar from "@/feature/MyInfo/Sidebar";
 import MyInfoView from "@/feature/MyInfo/MyInfoView";
 import ReservationView from "@/feature/MyInfo/ReservationView";
@@ -9,14 +11,25 @@ import ReservaionStatusView from "@/feature/MyInfo/ReservaionStatusView";
 import type { SidebarMenu } from "@/types/SidebarTypes";
 
 export default function MyInfoPage() {
-  const [activeMenu, setActiveMenu] = useState<SidebarMenu>("MY_INFO");
+  const searchParams = useSearchParams();
+  const router = useRouter();
+
+  const queryMenu = searchParams.get("menu") as SidebarMenu | null;
+
+  const [activeMenu, setActiveMenu] = useState<SidebarMenu>(
+    queryMenu || "MY_INFO"
+  );
+
+  const handleMenuChange = (menu: SidebarMenu) => {
+    setActiveMenu(menu);
+    router.push(`/myinfo?menu=${menu}`);
+  };
 
   return (
-    <div className="min-h-screen ">
-      {/* 중앙 컨테이너 */}
+    <div className="min-h-screen">
       <div className="mx-auto max-w-245 px-6 pt-10">
         <div className="flex gap-12.5">
-          <Sidebar active={activeMenu} onChange={setActiveMenu} />
+          <Sidebar active={activeMenu} onChange={handleMenuChange} />
 
           {/* 콘텐츠 영역 */}
           <main className="flex-1 min-w-0">
