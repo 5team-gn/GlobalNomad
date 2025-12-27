f68b13ece061ab317ecf28a8c5d918f509b9d8e4
DESKTOP-6FVFHUO\기훈 <tjsrlgns97@gmail.com>
2025-12-27 13:14:03 +0900

    MyInfo페이지 쿼리스트링 재적용

--- app/myinfo/page.tsx
+++ app/myinfo/page.tsx
@@ -1,36 +1,10 @@
-"use client";
-
-import { useState } from "react";
-
-import Sidebar from "@/feature/MyInfo/Sidebar";
-import MyInfoView from "@/feature/MyInfo/MyInfoView";
-import ReservationView from "@/feature/MyInfo/ReservationView";
-import MyExperinenceView from "@/feature/MyInfo/MyExperinenceView";
-import ReservaionStatusView from "@/feature/MyInfo/ReservaionStatusView";
-import type { SidebarMenu } from "@/types/SidebarTypes";
+import { Suspense } from "react";
+import MyInfoClient from "@/feature/MyInfo/MyInfoClient";
 
 export default function MyInfoPage() {
-  const [activeMenu, setActiveMenu] = useState<SidebarMenu>("MY_INFO");
-
-  const handleMenuChange = (menu: SidebarMenu) => {
-    setActiveMenu(menu);
-  };
-
   return (
-    <div className="min-h-screen">
-      <div className="mx-auto max-w-245 px-6 pt-10">
-        <div className="flex gap-12.5">
-          <Sidebar active={activeMenu} onChange={handleMenuChange} />
-
-          {/* 콘텐츠 영역 */}
-          <main className="flex-1 min-w-0">
-            {activeMenu === "MY_INFO" && <MyInfoView />}
-            {activeMenu === "RESERVATIONS" && <ReservationView />}
-            {activeMenu === "MY_EXPERIENCE" && <MyExperinenceView />}
-            {activeMenu === "RESERVATION_STATUS" && <ReservaionStatusView />}
-          </main>
-        </div>
-      </div>
-    </div>
+    <Suspense fallback={<div>로딩중...</div>}>
+      <MyInfoClient />
+    </Suspense>
   );
 }
