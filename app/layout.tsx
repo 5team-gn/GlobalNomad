15c69ea1df8fa8f29a3f8722a12a50b3267a206f
Jaeseung <kimchi90216@gmail.com>
2025-12-30 12:40:16 +0900

    Fix - 재승 : layout 오류 수정

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,33 +1,21 @@
-<<<<<<< HEAD
-
-=======
->>>>>>> 9a43604e0a273a268aa0721f5e1ab32fa3b6ac27
-import type { Metadata } from 'next';
+import type { Metadata } from "next";
+import "../style/global.css";
 
 export const metadata: Metadata = {
-  title: 'Global Nomad',
-  description: 'Global Nomad - 체험 예약 플랫폼',
+  title: "Design System Docs",
+  description: "Button component documentation",
 };
 
 export default function RootLayout({
   children,
-<<<<<<< HEAD
-
-=======
->>>>>>> 9a43604e0a273a268aa0721f5e1ab32fa3b6ac27
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="ko">
-      <body className="font-pretendard antialiased">
+      <body>
         {children}
       </body>
     </html>
   );
-<<<<<<< HEAD
-
 }
-=======
-}
->>>>>>> 9a43604e0a273a268aa0721f5e1ab32fa3b6ac27
