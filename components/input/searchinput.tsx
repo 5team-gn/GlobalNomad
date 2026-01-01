2acbb6919caeac97c206179af302a7f462b4e028
Choi hee ruk <chlgmlfkr593@naver.com>
2026-01-01 22:58:57 +0900

    ♻️ Refactor: SearchInput 스타일 단순화 및 공통 Input 활용

--- components/input/searchinput.tsx
+++ components/input/searchinput.tsx
@@ -5,10 +5,7 @@ import type { ComponentProps } from "react";
 export function SearchInput(props: ComponentProps<"input">) {
   return (
     <div className="relative w-full">
-      <Input
-        {...props}
-        className="h-[54px] w-full rounded-[16px] border border-gray-200 px-[20px] pl-[44px]"
-      />
+      <Input {...props} className="pl-[44px]" />
 
       <span className="pointer-events-none absolute left-4 top-1/2 -translate-y-1/2">
         <Image src="/icon_search.svg" alt="검색" width={20} height={20} />
