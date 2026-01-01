1794f3883d2c153fa9be7d31f1af16560acef152
Choi hee ruk <chlgmlfkr593@naver.com>
2026-01-01 23:05:53 +0900

    ♻️ Refactor: Input 컴포넌트에 forwardRef 적용

--- components/input/Input.tsx
+++ components/input/Input.tsx
@@ -1,15 +1,20 @@
-import { ComponentProps } from "react";
+import { ComponentProps, forwardRef } from "react";
 import clsx from "clsx";
 
-export function Input({ className, ...props }: ComponentProps<"input">) {
-  return (
-    <input
-      {...props}
-      className={clsx(
-        "h-[54px] w-full rounded-[16px] border border-gray-200 px-[20px] text-sm outline-none",
-        "focus:ring-1 focus:ring-primary-500",
-        className
-      )}
-    />
-  );
-}
+export const Input = forwardRef<HTMLInputElement, ComponentProps<"input">>(
+  ({ className, ...props }, ref) => {
+    return (
+      <input
+        ref={ref}
+        {...props}
+        className={clsx(
+          "h-[54px] w-full rounded-[16px] border border-gray-200 px-[20px] text-sm outline-none",
+          "focus:ring-1 focus:ring-primary-500",
+          className
+        )}
+      />
+    );
+  }
+);
+
+Input.displayName = "Input";
