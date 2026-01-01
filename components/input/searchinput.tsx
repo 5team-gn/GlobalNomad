cd9be8a80595d9d596c18cf357e4ed0103ad8157
Choi hee ruk <chlgmlfkr593@naver.com>
2026-01-01 22:49:55 +0900

    ♻️ Refactor: SearchInput 컴포넌트 구조 단순화 및 스타일 통합

--- components/input/searchinput.tsx
+++ components/input/searchinput.tsx
@@ -2,16 +2,15 @@ import Image from "next/image";
 import { Input } from "./Input";
 import type { ComponentProps } from "react";
 
-interface SearchInputProps extends ComponentProps<"input"> {
-  className?: string;
-}
-
-export function SearchInput({ className, ...props }: SearchInputProps) {
+export function SearchInput(props: ComponentProps<"input">) {
   return (
-    <div className="relative">
-      <Input {...props} className={`pl-10 ${className ?? ""}`} />
+    <div className="relative w-full">
+      <Input
+        {...props}
+        className="h-[54px] w-full rounded-[16px] border border-gray-200 px-[20px] pl-[44px]"
+      />
 
-      <span className="pointer-events-none absolute left-3 top-1/2 -translate-y-1/2">
+      <span className="pointer-events-none absolute left-4 top-1/2 -translate-y-1/2">
         <Image src="/icon_search.svg" alt="검색" width={20} height={20} />
       </span>
     </div>
