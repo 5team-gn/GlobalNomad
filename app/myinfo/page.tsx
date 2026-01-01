9543318aa750cc50fb9421c4519b01a181953e16
Choi hee ruk <chlgmlfkr593@naver.com>
2026-01-01 21:31:09 +0900

    🎨 Style: MyInfo 페이지 p-6 패딩 제거

--- app/myinfo/page.tsx
+++ app/myinfo/page.tsx
@@ -16,7 +16,7 @@ export default function MyInfoPage() {
       <div className="flex ml-30 xl:ml-117.5 pt-10">
         <Sidebar active={activeMenu} onChange={setActiveMenu} />
 
-        <main className="flex-1 p-6">
+        <main className="flex-1">
           {activeMenu === "MY_INFO" && <MyInfoView />}
           {activeMenu === "RESERVATIONS" && <ReservationView />}
           {activeMenu === "MY_EXPERIENCE" && <MyExperinenceView />}
