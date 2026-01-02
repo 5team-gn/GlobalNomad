18eebb0905486b7df07b59257ae3eea397941e29
Choi hee ruk <chlgmlfkr593@naver.com>
2026-01-03 00:29:52 +0900

    ♻️ Refactor: Input 컴포넌트에서 forwardRef 제거

--- components/input/Input.tsx
+++ components/input/Input.tsx
@@ -1,20 +1,19 @@
-import { ComponentProps, forwardRef } from "react";
+import { ComponentProps } from "react";
 import clsx from "clsx";
 
-export const Input = forwardRef<HTMLInputElement, ComponentProps<"input">>(
-  ({ className, ...props }, ref) => {
-    return (
-      <input
-        ref={ref}
-        {...props}
-        className={clsx(
-          "h-[54px] w-full rounded-[16px] border border-gray-200 px-[20px] text-sm outline-none",
-          "focus:ring-1 focus:ring-primary-500",
-          className
-        )}
-      />
-    );
-  }
-);
+type InputProps = ComponentProps<"input"> & {
+  inputRef?: React.Ref<HTMLInputElement>;
+};
 
-Input.displayName = "Input";
+export function Input({ className, inputRef, ...props }: InputProps) {
+  return (
+    <input
+      ref={inputRef}
+      {...props}
+      className={clsx(
+        "h-[54px] w-full rounded-[16px] border px-[20px] text-sm outline-none",
+        className
+      )}
+    />
+  );
+}
