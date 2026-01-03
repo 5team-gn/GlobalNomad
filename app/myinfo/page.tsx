a220ced2f88ae412350d5b10cb120c70f11e41c4
DESKTOP-6FVFHUO\기훈 <tjsrlgns97@gmail.com>
2026-01-03 13:55:11 +0900

    page.tsx 수정

--- app/myinfo/page.tsx
+++ app/myinfo/page.tsx
@@ -3,17 +3,8 @@ import MyInfoClient from "@/feature/MyInfo/MyInfoClient";
 
 export default function MyInfoPage() {
   return (
-    <div className="flex min-h-screen">
-      <div className="flex ml-30 xl:ml-117.5 pt-10">
-        <Sidebar active={activeMenu} onChange={setActiveMenu} />
-
-        <main className="flex-1">
-          {activeMenu === "MY_INFO" && <MyInfoView />}
-          {activeMenu === "RESERVATIONS" && <ReservationView />}
-          {activeMenu === "MY_EXPERIENCE" && <MyExperinenceView />}
-          {activeMenu === "RESERVATION_STATUS" && <ReservaionStatusView />}
-        </main>
-      </div>
-    </div>
+    <Suspense>
+      <MyInfoClient />
+    </Suspense>
   );
 }
