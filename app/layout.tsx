919eddd326f83511f81627cedd17cc37894a3658
Choi hee ruk <chlgmlfkr593@naver.com>
2026-01-05 20:46:30 +0900

    ✨ Feat: 전역 레이아웃에 react-hot-toast Toaster 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,11 @@
 import type { Metadata } from "next";
 import "../style/global.css";
+import { Toaster } from "react-hot-toast";
 
 export const metadata: Metadata = {
-  title: "Design System Docs",
-  description: "Button component documentation",
+  title: "GlobalNomad",
+  description:
+    "캘린더와 지도 SDK를 활용해 체험 상품을 예약할 수 있는 글로벌 체험 플랫폼",
 };
 
 export default function RootLayout({
@@ -15,6 +17,7 @@ export default function RootLayout({
     <html lang="ko">
       <body>
         {children}
+        <Toaster position="top-center" />
       </body>
     </html>
   );
