1e273b9cec0051d4f02b879bd81133e7be1642ed
Choi hee ruk <chlgmlfkr593@naver.com>
2026-01-05 21:43:32 +0900

    ♻️ Refactor: ReservationList 역할 단순화 및 책임 분리

--- feature/MyInfo/reservationview/ReservationList.tsx
+++ feature/MyInfo/reservationview/ReservationList.tsx
@@ -1,38 +1,18 @@
 "use client";
 
-import type {
-  Reservation,
-  ReservationStatus,
-} from "@/types/reservationview.types";
+import type { Reservation } from "@/types/reservationview.types";
 import { ReservationCard } from "./ReservationCard";
-import { ReservationEmpty } from "./ReservationEmpty";
 
 type Props = {
   reservations: Reservation[];
-  filter: ReservationStatus;
   onCancel: (reservation: Reservation) => void;
   onReview: (reservation: Reservation) => void;
 };
 
-export function ReservationList({
-  reservations,
-  filter,
-  onCancel,
-  onReview,
-}: Props) {
-  if (reservations.length === 0) {
-    return <ReservationEmpty type="all" />;
-  }
-
-  const filtered = reservations.filter((r) => r.status === filter);
-
-  if (filtered.length === 0) {
-    return <ReservationEmpty type="filtered" filter={filter} />;
-  }
-
+export function ReservationList({ reservations, onCancel, onReview }: Props) {
   return (
     <div className="space-y-[26px]">
-      {filtered.map((item) => (
+      {reservations.map((item) => (
         <ReservationCard
           key={item.id}
           reservation={item}
