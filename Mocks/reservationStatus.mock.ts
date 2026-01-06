1e46bb3c3f1d6c633756c26d0a742eb75159f5fd
DESKTOP-6FVFHUO\기훈 <tjsrlgns97@gmail.com>
2026-01-07 02:10:46 +0900

    mockdata  id string 형식으로 전환

--- Mocks/reservationStatus.mock.ts
+++ Mocks/reservationStatus.mock.ts
@@ -18,7 +18,7 @@ export const mockReservations: Reservation[] = [
     status: "pending",
   },
   {
-    id: 2,
+    id: "오승환",
     title: "내 강아지 인생 사진 찍어주기",
     date: "2026-02-11T13:00:00",
     price: 35000,
@@ -26,7 +26,7 @@ export const mockReservations: Reservation[] = [
     status: "canceled",
   },
   {
-    id: 3,
+    id: "이대호",
     title: "이색 액티비티 체험",
     date: "2026-01-10T10:00:00",
     price: 60000,
@@ -34,7 +34,7 @@ export const mockReservations: Reservation[] = [
     status: "declined",
   },
   {
-    id: 4,
+    id: "양의지",
     title: "별과 함께하는 북촌 체험",
     date: "2026-01-14T15:00:00",
     price: 40000,
