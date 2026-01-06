c2d921a763c71b6e40980ead0b2f19daf3f37f31
DESKTOP-6FVFHUO\기훈 <tjsrlgns97@gmail.com>
2026-01-07 02:13:22 +0900

    예약 상태 코드 및 스타일 맵 수정

--- feature/reservationStatus/Calendar/StatusBadge.tsx
+++ feature/reservationStatus/Calendar/StatusBadge.tsx
@@ -1,21 +1,27 @@
+import {
+  ReservationStatusCode,
+  RESERVATION_STATUS_LABEL,
+} from "@/feature/reservationStatus/types/reservationStatus";
+
 interface StatusBadgeProps {
-  status: "예약" | "승인" | "완료";
+  status: ReservationStatusCode;
   count: number;
 }
 
-export function StatusBadge({ status, count }: StatusBadgeProps) {
-  const styleMap = {
-    예약: "bg-blue-50 text-blue-500",
-    승인: "bg-orange-50 text-orange-500",
-    완료: "bg-gray-100 text-gray-500",
-  };
+const STYLE_MAP: Record<ReservationStatusCode, string> = {
+  pending: "bg-blue-50 text-blue-500",
+  confirmed: "bg-orange-50 text-orange-500",
+  declined: "bg-red-50 text-red-500",
+  canceled: "bg-gray-100 text-gray-400",
+  completed: "bg-gray-100 text-gray-500",
+};
 
+export function StatusBadge({ status, count }: StatusBadgeProps) {
   return (
     <div
-      className={`rounded-md px-2.5 py-0.5 text-14-m  ${styleMap[status]}`}
+      className={`rounded-md px-2.5 py-0.5 text-14-m ${STYLE_MAP[status]}`}
     >
-      {status} {count}
+      {RESERVATION_STATUS_LABEL[status]} {count}
     </div>
   );
 }
-
