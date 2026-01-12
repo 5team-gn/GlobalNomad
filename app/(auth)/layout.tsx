95005eb009dce05d4ebe8ec7df58d69bbcf6ca5c
DESKTOP-6FVFHUO\기훈 <tjsrlgns97@gmail.com>
2026-01-13 00:23:11 +0900

    재수정 {auth} 폴더 사용

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,6 +1,6 @@
 export default function AuthLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex bg-gray-50 min-h-screen items-center justify-center">
+    <div className="bg-gray-50 items-center justify-center">
       {children}
     </div>
   );
