679411253f9b2b95b33f90c0290ab28f436206d4
DESKTOP-KDUIRM0\jukma <jukman81@gmail.com>
2026-01-12 10:48:33 +0900

    파일 변경전으로 복원

--- feature/activities-detail/api/getActivityDetail.ts
+++ feature/activities-detail/api/getActivityDetail.ts
@@ -1,9 +1,16 @@
 import { apiFetch } from "@/lib/api/apiFetch";
-import { TEAM_ID } from "@/constants/env";
-import { ActivityDetailResponse } from "@/types/activities/activity.types";
+import { ActivityDetail } from "@/types/activities/activity.types";
+
+function getTeamId() {
+  const teamId = process.env.NEXT_PUBLIC_TEAM_ID;
+  if (!teamId) throw new Error("팀아이디 없음");
+  return teamId;
+}
 
 export async function getActivityDetail(activityId: number) {
-  return apiFetch<ActivityDetailResponse>(`/${TEAM_ID}/activities/${activityId}`, {
+  const teamId = getTeamId();
+  if (!teamId) throw new Error("팀아이디 없음");
+  return apiFetch<ActivityDetail>(`/${teamId}/activities/${activityId}`, {
     cache: "no-store",
   });
 }
