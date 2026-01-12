7aa8d38bb79293046116b4b8f34e8b1bf5feb57c
DESKTOP-6FVFHUO\기훈 <tjsrlgns97@gmail.com>
2026-01-12 23:58:19 +0900

    리뷰반영 layout 일부 수정

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,6 +1,6 @@
 export default function AuthLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="bg-gray-50 min-h-screen">
+    <div className="flex bg-gray-50 min-h-screen items-center justify-center">
       {children}
     </div>
   );
--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -3,7 +3,7 @@ import Footer from "@/components/footer/footer";
 
 export default function MainLayout({children}:{children: React.ReactNode}) {
     return (
-        <div>
+        <div className="flex flex-col min-h-screen">
             <Header/>
             <main className="grow">{children}</main>
             <Footer/>
